2f39d9f855965077d503639dc2347a1683ad464a
Fernando Ortega Martinez <fomdeveloper@gmail.com>
2017-02-19 12:53:21 +0000

    Add Fabric Crashlytics.

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -87,6 +87,7 @@
 }
 
 # Crashlytics
+-keepattributes SourceFile,LineNumberTable
 -keep public class * extends java.lang.Exception
 -keep class com.crashlytics.** { *; }
 -dontwarn com.crashlytics.**
--- app/src/main/java/com/fomdeveloper/planket/PlanketApplication.java
+++ app/src/main/java/com/fomdeveloper/planket/PlanketApplication.java
@@ -2,10 +2,13 @@ package com.fomdeveloper.planket;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.fomdeveloper.planket.injection.AppComponent;
 import com.fomdeveloper.planket.injection.AppModule;
 import com.fomdeveloper.planket.injection.DaggerAppComponent;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
@@ -18,6 +21,12 @@ public class PlanketApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        Fabric.with(this, crashlyticsKit);
 
         if (BuildConfig.DEBUG){
             Timber.plant(new Timber.DebugTree());
